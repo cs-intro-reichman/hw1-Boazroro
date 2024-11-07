1f7e4f1d3c19b786a4d4c7c65525eaf7d9b56e32
unknown <boaz6151@gmail.com>
2024-11-07 07:50:53 +0200

    Completed

--- NumWords.java
+++ NumWords.java
@@ -3,13 +3,23 @@ public class NumWords {
 	public static void main(String args[]) {
 	    
 		 int number = Integer.parseInt(args[0]);
-        
-		
-		 int hundreds = (number / 100) % 10;  // Get the hundreds place
-		 int tens = (number / 10) % 10;       // Get the tens place
-		 int ones = number % 10;              // Get the ones place
-		 int higherHundreds = number / 100;
-		 // Print the result in the specified format
-		 System.out.println( higherHundreds + hundreds + " hundreds, " + tens + " tens, and " + ones + " ones.");
-	}
+		 int hundreds = (number / 100) % 10; 
+		 int tens = (number / 10) % 10;       
+		 int ones = number % 10;      
+		 if (number>=1000) 
+		 {
+			int higherhundreds =  number / 100 ;
+			 System.out.println( higherhundreds + hundreds + hundreds + " hundreds, " + tens + " tens, and " + ones + " ones.");
+
+
+		 } 
+		 
+		 else
+		 
+		 {       
+		 
+		 System.out.println( hundreds + " hundreds, " + tens + " tens, and " + ones + " ones.");
+		 }
+
+		}
 }
