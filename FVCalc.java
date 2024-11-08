b6e29016e69c91bbb9bd157294feabc29b109536
unknown <boaz6151@gmail.com>
2024-11-08 10:03:08 +0200

    Completed

--- FVCalc.java
+++ FVCalc.java
@@ -2,12 +2,12 @@
 public class FVCalc {
 	public static void main(String[] args){
 		
-		int currentValue = Integer.parseInt(args[0]);
-		double interest = Double.parseDouble(args[1])/100;
+		int currentValue1 = Integer.parseInt(args[0]);
+		double interest1 = Double.parseDouble(args[1])/100;
 		int years = Integer.parseInt(args[2]);
-		double futurevalue = currentValue * Math.pow(interest + 1 ,years);
-        System.out.println("After " + years + " years" + ", a" + " $" +currentValue + 
-		         " saved at " + (100 * interest) +"% will yield " + "$" + ((int)futurevalue));
+		double futurevalue = currentValue1 * Math.pow(interest1 + 1 ,years);
+        System.out.println("After " + years + " years" + ", a" + " $" + currentValue1 + 
+		         " saved at " + (100 * interest1) +"% will yield " + "$" + ((int)futurevalue));
 		
 
 		
